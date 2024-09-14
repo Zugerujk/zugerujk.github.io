3b332e640df3dbd02a639ad0b8198dc37740922c
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2024-09-14 05:43:33 -0500

    v1.1.1
    Adds blog055 and related media. Updates blogtemplate.

--- blog_construct_code.js
+++ blog_construct_code.js
@@ -460,6 +460,13 @@ var page_listing = {
         link: "https://zugerujk.net/blog/blog054",
         var: "3",
     },
+    "054": {
+        displaynum: "#055",
+        title: "Magdalena Bay Imaginal Mystery Tour Fall 2024",
+        date: "9/14/24 5:28:19 AM CST",
+        link: "https://zugerujk.net/blog/blog055",
+        var: "3",
+    },
 }
 
 var included_pages = [];
