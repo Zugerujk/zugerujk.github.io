0134f7201a682c381f877911728e28fa8c867c4b
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2025-04-23 08:59:41 -0500

    v1.2.9
    Adds blog075.

--- blog_construct_code.js
+++ blog_construct_code.js
@@ -600,6 +600,13 @@ var page_listing = {
         link: "https://zugerujk.net/blog/blog074",
         var: "3",
     },
+    "074": {
+        displaynum: "#075",
+        title: "I feel weird about writing Yo • to • Yo • ta",
+        date: "4/23/25 8:56:27 AM CST",
+        link: "https://zugerujk.net/blog/blog075",
+        var: "3",
+    },
 }
 
 var included_pages = [];
