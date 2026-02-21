528e1ce1bda7aa7891d150681575e917a41429ca
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2026-02-21 00:37:13 -0600

    v1.3.1
    Adds an RSS feed, support for an RSS feed, and related media.

--- universal_cooooode.js
+++ universal_cooooode.js
@@ -294,7 +294,7 @@ const createNavbar = function(blocked_space_arg = "") {
     FOOTER STUFF
 */
 const createFooter = function() {
-    let generic_footer_text = "All rights reserved, Me, 2026. v1.3.0";
+    let generic_footer_text = "All rights reserved, Me, 2026. v1.3.1";
     let settings_text = "⚙️ Settings";
     
     // Find the navbar div
